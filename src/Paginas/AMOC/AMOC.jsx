033224e9c26b2d085d83fa2e5f76d32dc38dfec6
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-07 12:33:53 -0300

    pagina de contato

--- src/Paginas/AMOC/AMOC.jsx
+++ src/Paginas/AMOC/AMOC.jsx
@@ -2,7 +2,7 @@ import contru from './contrucao.png'
 function AMOC() {
     return (  
         <div className='AMOC'>
-            <h1>em construcao</h1>
+            <h1>em construção</h1>
 
             <img src={contru} alt="" />
         </div>
--- src/Paginas/Contato/Contato.jsx
+++ src/Paginas/Contato/Contato.jsx
@@ -1,40 +1,113 @@
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
-import { dadosContato } from './dadosContato.jsx'
+// import { GoogleLogin } from '@react-oauth/google';
+// import { jwtDecode } from "jwt-decode";
+// <GoogleLogin
+// onSuccess={credentialResponse => {
+//     const decoded = jwtDecode(credentialResponse?.credential);
+//     console.log(decoded);
+// }}
+// onError={() => {
+//     console.log('Login Failed');
+// }}
+// />
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function Contato() {
-    return (
-        <div className='Contato'>
-            <h1>contato</h1>
+  const [validated, setValidated] = useState(false);
 
-            <div className="form">
-                
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-                {dadosContato.map(dado => (
+    setValidated(true);
+  };
 
-                    <div  className='iconDiv' key={dado.id}>
-                    
-                        <ion-icon name={dado.icon} />
-                    
-                        <input type="text" placeholder={dado.placeHolder} />
-                    </div>
-                ))}
+  return (
+    <Form className='Contato' noValidate validated={validated} onSubmit={handleSubmit}>
+      
+      <Col className="mb-3 divContato">
 
-                <button>enviar</button>
+        <Form.Group as={Col} controlId="validationCustom01">
+          <Form.Control
+            required
+            type="text"
+            placeholder="Primeiro nome"
+            defaultValue=""
+          />
 
-                <GoogleLogin
-                    onSuccess={credentialResponse => {
-                        const decoded = jwtDecode(credentialResponse?.credential);
-                        console.log(decoded);
-                    }}
-                    onError={() => {
-                        console.log('Login Failed');
-                    }}
-                />
-            </div>
+          <Form.Control.Feedback>Boa!!</Form.Control.Feedback>
 
-        </div>
-    );
+        </Form.Group>
+        
+        <Form.Group as={Col} controlId="validationCustom02">
+          
+          
+          <Form.Control
+            required
+            type="text"
+            placeholder="Ultimo nome"
+            defaultValue=""
+          />
+          
+          <Form.Control.Feedback>Boa!!</Form.Control.Feedback>
+        </Form.Group>
+        
+        <Form.Group as={Col} controlId="validationCustomUsername">
+        
+        
+          <InputGroup className='Email' hasValidation>
+        
+            <InputGroup.Text className='iconeMail' id="inputGroupPrepend">@</InputGroup.Text>
+        
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+        
+            <Form.Control.Feedback type="invalid">
+              Por favor, coloque seu E-mail
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="validationCustom02">
+          
+          
+        <Form.Control
+          as="textarea"
+          placeholder="Escreva sua mensagem..."
+          style={{ height: '100px' }}
+
+          required
+          defaultValue=""
+        />
+          
+          <Form.Control.Feedback>Obrigado pelo seu feedBack</Form.Control.Feedback>
+        </Form.Group>
+      </Col>
+      
+      <Form.Group className="mb-3">
+        <Form.Check
+          required
+          label="Aceita os termos de condição"
+          feedback="É necessário aceitar"
+          feedbackType="invalid"
+          
+        />
+      </Form.Group>
+
+
+      <Button type="submit" onClick={() => (alert('mensagem enviada com sucesso'))}>Enviar</Button>
+    </Form>
+  );
 }
 
 export default Contato;
