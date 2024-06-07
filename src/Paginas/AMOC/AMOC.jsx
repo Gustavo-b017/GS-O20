1c778a8bfd5bbb174dd1291ad04c22f004dba48b
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-07 10:52:32 -0300

    amoc

--- src/Paginas/AMOC/AMOC.jsx
+++ src/Paginas/AMOC/AMOC.jsx
@@ -1,9 +1,10 @@
+import contru from './contrucao.png'
 function AMOC() {
     return (  
         <div>
-            <h1>em construcao
-                
-            </h1>
+            <h1>em construcao</h1>
+
+            <img src={contru} alt="" />
         </div>
     );
 }
